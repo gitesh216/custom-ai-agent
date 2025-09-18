266eff225608543580adafca1f4bf941a37cdae0
gitesh216 <109015479+gitesh216@users.noreply.github.com>
2025-09-18 15:14:22 +0530

    feat: add websearch tool function

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 import { ChatGroq } from "@langchain/groq";
 import { MessagesAnnotation, StateGraph } from "@langchain/langgraph";
 import readline from "node:readline/promises";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
+import { TavilySearch } from "@langchain/tavily";
+
+const webSearchTool = new TavilySearch({
+    max_results: 5,
+    topic: "general",
+    // include_answer=False,
+    // include_raw_content=False,
+    // include_images=False,
+    // include_image_descriptions=False,
+    // search_depth="basic",
+    // time_range="day",
+    // include_domains=None,
+    // exclude_domains=None
+});
+
+/**
+ * Initialize Tool node
+ */
+const tools = [webSearchTool]
+const toolNode = new ToolNode(tools);
 
 /**
  * 1. Define node function
@@ -16,7 +37,7 @@ const llm = new ChatGroq({
   temperature: 0,
   maxTokens: undefined,
   maxRetries: 2,
-});
+}).bindTools(tools);
 
 
 async function callModel(state) {
@@ -28,13 +49,27 @@ async function callModel(state) {
   return {messages: [response]};
 }
 
+function shouldContinue(state) {
+  // put your condition
+  // whether to call a tool or end
+  const lastMessage = state.messages[state.messages.length - 1];
+  // console.log(state);
+  if(lastMessage.tool_calls.length > 0) {
+    return 'tools';
+  }
+  return '__end__';
+}
+
 /**
  * Build the graph
  */
 const workflow = new StateGraph(MessagesAnnotation)
   .addNode("agent", callModel)
+  .addNode("tools", toolNode)
   .addEdge("__start__", "agent")
-  .addEdge("agent", "__end__");
+  .addEdge("agent", "__end__")
+  .addEdge("tools", "agent")
+  .addConditionalEdges("agent", shouldContinue);
 
 
 /**
